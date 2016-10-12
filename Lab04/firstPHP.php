78d728f4ba8eb1d902760b0dfe5fd5b83605e06a
Sishaar Rao <Sishaar@Sishaars-MacBook-Pro.local>
2016-10-12 12:56:59 -0400

    updated web folder

---
+++ Lab04/firstPHP.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP First Run</title>
+    </head>
+    <body>
+        <?php
+            echo("<input id='toOutput'>")
+        ?>
+        <button id="changeStuff"></button>   
+        <div id="outputDiv"></div>
+    </body>
+</html>
