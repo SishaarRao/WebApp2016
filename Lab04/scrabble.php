9aacf54dd8aa763e83086bb346b3c663fdf9bbb0
Sishaar Rao <Sishaar@Sishaars-MacBook-Pro.local>
2016-10-26 13:44:30 -0400

    updated lab 4

--- Lab04/scrabble.php
+++ Lab04/scrabble.php
@@ -1,6 +1,10 @@
 <?php
     $key = $_REQUEST["key"];
     $myfile = fopen("words.txt", "r") or die("Unable to open file!");
-    echo fread($myfile,filesize("words.txt"));
+    $results;
+    $pattern = "/\s+[".$key."]+\s/gm";
+    preg_match_all($pattern, $myfile, $results);
+    //echo fread($myfile,filesize("words.txt"));
+    echo($results);
     fclose($myfile);
 ?>
