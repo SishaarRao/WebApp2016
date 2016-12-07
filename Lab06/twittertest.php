fb7d2c65a67a86a1cf055024ec3382c5cdc4dd9f
Sishaar Rao <Sishaar@Sishaars-MBP.lan>
2016-12-06 22:33:13 -0500

    change

--- Lab06/twittertest.php
+++ Lab06/twittertest.php
@@ -1,7 +1,7 @@
 <?php
     require "twitteroauth/autoload.php";
     use Abraham\TwitterOAuth\TwitterOAuth;
-        
+    $name = $_REQUEST["name"];
     $conskey="dgCHShiDqLIvhksyVtFmUKwJz";
     $conssec="u6YLN2L8fRlvluAfQWW7OOKeJv3QSlxHihjatOkjYwvInwiRXD";
     $acctok="578176008-AVadeZ78brCTcpBFrlGAJ8tPuzqm94TLBGPgDGw6";
@@ -10,6 +10,6 @@
     $content = $connection->get("account/verify_credentials");
     //var_dump($content);
 
-    $statuses = $connection->get("statuses/user_timeline", ["q" => "twitterapi", "screen_name" => "SpideyLi"]);
-    var_dump($statuses);
+    $statuses = $connection->get("search/tweets", ["q" => "twitterapi", "screen_name" => $name]);
+    print_r($statuses);
 ?>
